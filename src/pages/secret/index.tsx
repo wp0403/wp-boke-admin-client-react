62a0df3d802d3ba48d9001d00237d0e7944c51c9
wengpeng0403 <89257284+wengpeng0403@users.noreply.github.com>
2022-07-13 10:02:20 +0800

    fix:

--- src/pages/secret/index.tsx
+++ src/pages/secret/index.tsx
@@ -4,7 +4,7 @@
  * @Author: WangPeng
  * @Date: 2022-06-08 13:51:46
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-07-12 18:23:24
+ * @LastEditTime: 2022-07-13 10:02:10
  */
 import React, { useState, useEffect, useRef } from 'react';
 import { history } from 'umi';
@@ -199,7 +199,7 @@ const Classify = (props: any) => {
     },
     {
       dataIndex: 'operation',
-      width: 100,
+      width: 120,
       fixed: 'right',
       render: (text, record) => (
         <div className={tableStyle.table_cell}>
