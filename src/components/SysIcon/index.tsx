6d9cb299fe46f29a6e97a812c6caa6d2d17bb20a
wengpeng0403 <89257284+wengpeng0403@users.noreply.github.com>
2022-07-13 10:25:25 +0800

    fix:

--- src/components/SysIcon/index.tsx
+++ src/components/SysIcon/index.tsx
@@ -2,7 +2,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 const SysIcon = createFromIconfontCN({
   scriptUrl:
-    '//wp-1302605407.cos.ap-beijing.myqcloud.com/font_2977360_nsfsg1logch/iconfont.js',
+    '//wp-1302605407.cos.ap-beijing.myqcloud.com/font_2977360_mylmwgf4iqk/iconfont.js',
 });
 
 export default SysIcon;
