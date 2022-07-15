e24c71f5c5e5e9b97ef1bd52b1d7417cc69d0392
wengpeng0403 <89257284+wengpeng0403@users.noreply.github.com>
2022-07-15 18:04:02 +0800

    fix:

--- src/api/config/home.ts
+++ src/api/config/home.ts
@@ -4,13 +4,14 @@
  * @Author: 张三
  * @Date: 2021-07-11 21:53:36
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-06-27 16:37:39
+ * @LastEditTime: 2022-07-15 13:57:50
  */
 // 首页轮播博文列表
 export const _getSwiperBowenList = {
   url: '/adminApi/getSwiperBowenList',
   method: 'get',
 };
+
 // 首页底部信息接口
 export const _getHomeFooter = {
   url: '/adminApi/getHomeFooter',
