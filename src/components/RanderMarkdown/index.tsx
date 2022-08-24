be93ecb2b0718fc3071c7a326852b4417ce72f28
王鹏 <75709526+wang0403@users.noreply.github.com>
2022-08-24 14:28:27 +0800

    fix:

--- src/components/RanderMarkdown/index.tsx
+++ src/components/RanderMarkdown/index.tsx
@@ -28,44 +28,46 @@ const RanderMarkdown = (props: any) => {
 
   return (
     <Fragment>
-      <ReactMarkdown
-        children={props.markdown}
-        className="markdown_body"
-        remarkPlugins={[remarkGfm, remarkMath, remarkToc]}
-        rehypePlugins={[rehypeRaw, rehypeKatex]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, '')}
-                style={coy}
-                language={match[1]}
-                showLineNumbers
-                PreTag="div"
-                {...props}
-              />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-          img({ src, alt }) {
-            return <Image src={src} alt={alt} />;
-          },
-          a({ href, children }) {
-            if (RegExp('#').test(href || '')) {
-              return <a href={href}>{children}</a>;
-            }
-            return (
-              <a href={href} target="_blank">
-                {children}
-              </a>
-            );
-          },
-        }}
-      />
+      <Image.PreviewGroup>
+        <ReactMarkdown
+          children={props.markdown}
+          className="markdown_body"
+          remarkPlugins={[remarkGfm, remarkMath, remarkToc]}
+          rehypePlugins={[rehypeRaw, rehypeKatex]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  style={coy}
+                  language={match[1]}
+                  showLineNumbers
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+            img({ src, alt }) {
+              return <Image src={src} alt={alt} />;
+            },
+            a({ href, children }) {
+              if (RegExp('#').test(href || '')) {
+                return <a href={href}>{children}</a>;
+              }
+              return (
+                <a href={href} target="_blank">
+                  {children}
+                </a>
+              );
+            },
+          }}
+        />
+      </Image.PreviewGroup>
     </Fragment>
   );
 };
