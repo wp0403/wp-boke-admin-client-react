c951f4b2c2f8c381fe0e2df3b2f817bf03b277a5
王鹏 <75709526+wp0403@users.noreply.github.com>
2022-09-13 11:09:44 +0800

    fix:

--- src/pages/itinerary-details/index.tsx
+++ src/pages/itinerary-details/index.tsx
@@ -1,19 +1,418 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: WangPeng
  * @Date: 2022-06-08 14:00:31
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-06-08 14:00:32
+ * @LastEditTime: 2022-09-13 11:07:17
  */
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, history } from 'umi';
+import {
+  Table,
+  Image,
+  Tooltip,
+  message,
+  Switch,
+  Button,
+  Popconfirm,
+} from 'antd';
+import { DeleteOutlined, UndoOutlined } from '@ant-design/icons';
+import type { ColumnsType, TablePaginationConfig } from 'antd/lib/table';
+import type { FilterValue, SorterResult } from 'antd/lib/table/interface';
+import { calcTableScrollWidth, formatDate } from '@/utils/dataUtils';
+import { getDictObj } from '@/utils/globalDataUtils';
+import api from '@/api';
+import SysIcon from '@/components/SysIcon';
+import ToExamineModal from '@/components/ToExamineModal';
+import tableStyle from '@/table.less';
+import style from './index.less';
 
-const ItineraryDetails = () => {
-    return (
-        <div>
-            
+const { classify } = api;
+
+interface DataType {
+  id: string;
+  timeData: string;
+  last_edit_time: string;
+  weatherId: string;
+  place: string;
+  moodId: string;
+  title: string;
+  content: string;
+  img: string;
+  imgs: string[];
+  isDelete: boolean;
+  type: number;
+}
+
+const Classify = (props: any) => {
+  // 弹窗抛出的事件
+  const modalRef = useRef();
+  // 是否精选事件
+  const changeSwitch = async (val, id) => {
+    await classify
+      ._changeClassifySelected({ id, selected: val })
+      .then(({ data }) => {
+        if (data.code === 200) {
+          setList((v) =>
+            v.map((item) =>
+              item.id === id ? { ...item, selected: val } : item,
+            ),
+          );
+          message.success(data.msg);
+        } else {
+          message.error(data.msg);
+        }
+      });
+  };
+  // 删除/恢复 博文事件
+  const delBowenObj = async (val, id) => {
+    await classify._delBowenList({ id, isDelete: !val }).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) =>
+          v
+            .map((item) =>
+              item.id === id ? { ...item, isDelete: !val } : item,
+            )
+            .filter((item) => (+type === 2 ? item.isDelete : !item.isDelete)),
+        );
+        message.success(data.msg);
+        +type === 2
+          ? setTotal((v) => (val ? (v -= 1) : (v += 1)))
+          : setTotal((v) => (val ? (v += 1) : (v -= 1)));
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+  // 彻底删除博文
+  const deleteBowenObj = async (id) => {
+    await classify._deleteClassifyDetails({ id }).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) => v.filter((item) => item.id !== id));
+        message.success(data.msg);
+        setTotal((v) => (v -= 1));
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+  // 修改博文审核状态
+  const changeToExamine = async (obj) => {
+    await classify._putClassifyToExamine(obj).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) =>
+          v.map((item) =>
+            item.id === obj.id ? { ...item, type: +obj.type } : item,
+          ),
+        );
+        (modalRef.current as any)?.handleCancel();
+        message.success(data.msg);
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title',
+      fixed: 'left',
+      render: (text, record) => (
+        <div className={tableStyle.table_cell}>
+          <Tooltip placement="topLeft" title={text}>
+            <Link
+              target="_blank"
+              style={{ cursor: 'pointer' }}
+              to={`/classify/${record.id}/details`}
+            >
+              {text}
+            </Link>
+          </Tooltip>
         </div>
-    );
+      ),
+    },
+    {
+      title: '一级类',
+      dataIndex: 'classify',
+      key: 'classify_id',
+      width: 160,
+      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+    },
+    {
+      title: '二级类',
+      dataIndex: 'classify_sub',
+      key: 'classify_sub_id',
+      width: 160,
+      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+    },
+    {
+      title: '博文状态',
+      dataIndex: 'type',
+      key: 'type',
+      width: 160,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          <SysIcon
+            type={getDictObj('toExamine_type', text)?.icon}
+            style={{ marginRight: '10px' }}
+          />
+          <span
+            className={
+              text === 1
+                ? style.bowenType1
+                : text === 2
+                ? style.bowenType2
+                : text === 3
+                ? style.bowenType3
+                : ''
+            }
+          >
+            {getDictObj('toExamine_type', text)?.value}
+          </span>
+        </div>
+      ),
+    },
+    {
+      title: '内容简介',
+      dataIndex: 'desc',
+      key: 'desc',
+      width: 300,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          <Tooltip placement="topLeft" title={text}>
+            {text}
+          </Tooltip>
+        </div>
+      ),
+    },
+    {
+      title: '文档类型',
+      dataIndex: 'storage_desc',
+      key: 'storage_type',
+      width: 120,
+      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'time_str',
+      key: 'time_str',
+      width: 220,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          {formatDate(text, 'yyyy-MM-dd HH:ss:mm')}
+        </div>
+      ),
+    },
+    {
+      title: '最后修改时间',
+      dataIndex: 'last_edit_time',
+      key: 'last_edit_time',
+      width: 220,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          {formatDate(text, 'yyyy-MM-dd HH:ss:mm')}
+        </div>
+      ),
+    },
+    {
+      title: '列表图片',
+      dataIndex: 'img',
+      key: 'img',
+      width: 100,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          <Image className={style.img_list} src={text} />
+        </div>
+      ),
+    },
+    {
+      title: '是否精选',
+      dataIndex: 'selected',
+      key: 'selected',
+      width: 100,
+      render: (text, record) => (
+        <div className={tableStyle.table_cell}>
+          <Switch checked={text} onChange={(v) => changeSwitch(v, record.id)} />
+        </div>
+      ),
+    },
+    {
+      title: '作者',
+      dataIndex: 'author',
+      key: 'author_id',
+      width: 120,
+      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      width: 120,
+      fixed: 'right',
+      render: (text, record) => (
+        <div className={tableStyle.table_cell_flex}>
+          {+type !== 2 && (
+            <Tooltip
+              placement="topRight"
+              arrowPointAtCenter
+              title="修改当前博文审核状态"
+            >
+              <SysIcon
+                type="icon-shenhe"
+                className={style.btn_huifu}
+                onClick={() =>
+                  (modalRef.current as any)?.showModal({
+                    ...record,
+                    type: `${record.type}`,
+                  })
+                }
+              />
+            </Tooltip>
+          )}
+          {+type === 2 ? (
+            <Tooltip
+              placement="topRight"
+              arrowPointAtCenter
+              title="点击后将恢复到列表中"
+            >
+              <UndoOutlined
+                className={style.btn_huifu}
+                onClick={() => delBowenObj(record.isDelete, record.id)}
+              />
+            </Tooltip>
+          ) : (
+            <Popconfirm
+              title="真的要删除吗？删除后将不能在网站查看。"
+              onConfirm={() => delBowenObj(record.isDelete, record.id)}
+              okText="确定"
+              cancelText="取消"
+              placement="topRight"
+              arrowPointAtCenter
+            >
+              <DeleteOutlined className={style.btn_remove} />
+            </Popconfirm>
+          )}
+          {+type === 2 && (
+            <Popconfirm
+              title="将彻底删除该条数据，不可恢复，要继续吗？"
+              onConfirm={() => deleteBowenObj(record.id)}
+              okText="确定"
+              cancelText="取消"
+              placement="topRight"
+              arrowPointAtCenter
+            >
+              <DeleteOutlined className={style.btn_remove} />
+            </Popconfirm>
+          )}
+        </div>
+      ),
+    },
+  ];
+
+  const { type } = props.location.query as any;
+  const [list, setList] = useState<DataType[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  // 请求列表数据事件
+  const getList = async (obj?) => {
+    setLoading(true);
+    await classify
+      ._getClassifyList({
+        params: { isDelete: +type === 2 ? 0 : 1, page, pageSize },
+      })
+      .then(({ data }) => {
+        if (data.code === 200) {
+          setList(data.data);
+          setTotal(data.meta.total);
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+  // 表格的change事件
+  const handleTableChange = (
+    { current, pageSize },
+    selectedRows,
+    info: { type },
+  ) => {
+    setPage(current);
+    setPageSize(pageSize);
+  };
+  // 跳转页面事件
+  const changePageType = () => {
+    setPage(1);
+    if (+type === 2) {
+      history.replace('/classify/list');
+    } else {
+      history.replace('/classify/list?type=2');
+    }
+  };
+
+  // 初始化列表
+  useEffect(() => {
+    getList();
+  }, [type, page, pageSize]);
+
+  return (
+    <div className={style.classify}>
+      <div className={style.headerBox}>
+        <div className={style.headerBox_left}>
+          <span className={style.page_title}>
+            博文{+type === 2 ? '回收站' : '列表页'}
+          </span>{' '}
+          <span className={style.page_total}>{total}</span>
+        </div>
+        <div className={style.headerBox_right}>
+          <Button
+            type="primary"
+            shape="round"
+            className={style.headerBox_right_btn}
+            href="/classify/add-bowen"
+          >
+            新增博文
+          </Button>
+          <Button
+            type="primary"
+            shape="round"
+            onClick={changePageType}
+            className={
+              +type === 2
+                ? style.headerBox_right_btn
+                : style.headerBox_right_btn1
+            }
+          >
+            {+type === 2 ? '列表页' : '回收站'}
+          </Button>
+        </div>
+      </div>
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={list}
+        rowKey={'id'}
+        scroll={{
+          scrollToFirstRowOnChange: true,
+          x: calcTableScrollWidth(columns),
+          y: `calc(100vh - 310px)`,
+        }}
+        pagination={{
+          current: page,
+          pageSize: pageSize,
+          total: total,
+          showTitle: false,
+        }}
+        onChange={handleTableChange as any}
+      />
+      <ToExamineModal
+        callback={(ref) => (modalRef.current = ref)}
+        name="type"
+        changeToExamine={changeToExamine}
+      />
+    </div>
+  );
 };
 
-export default ItineraryDetails;
+export default Classify;
--- src/pages/itinerary/index.tsx
+++ src/pages/itinerary/index.tsx
@@ -4,14 +4,26 @@
  * @Author: WangPeng
  * @Date: 2022-06-08 13:51:46
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-09-09 15:03:20
+ * @LastEditTime: 2022-09-13 11:06:30
  */
-import React, { useState, useEffect } from 'react';
-import { Link } from 'umi';
-import { Table, Image } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, history } from 'umi';
+import {
+  Table,
+  Image,
+  Tooltip,
+  message,
+  Switch,
+  Button,
+  Popconfirm,
+} from 'antd';
 import type { ColumnsType } from 'antd/lib/table';
+import { DeleteOutlined, UndoOutlined } from '@ant-design/icons';
 import { calcTableScrollWidth, formatDate } from '@/utils/dataUtils';
+import { getDictObj } from '@/utils/globalDataUtils';
 import api from '@/api';
+import ToExamineModal from '@/components/ToExamineModal';
+import SysIcon from '@/components/SysIcon';
 import tableStyle from '@/table.less';
 import style from './index.less';
 
@@ -19,25 +31,59 @@ const { itinerary } = api;
 
 interface DataType {
   id: string;
-  time_str: string;
+  timeData: string;
   last_edit_time: string;
-  img: string;
-  author: string;
-  author_id: string;
-  classify: string;
-  classify_id: string;
-  classify_sub: string;
-  classify_sub_id: string;
+  weatherId: string;
+  place: string;
+  moodId: string;
   title: string;
-  desc: string;
   content: string;
-  storage_type: string;
-  selected: string;
+  img: string;
+  imgs: string[];
+  isDelete: boolean;
+  type: number;
 }
 
-const Classify = () => {
+const Classify = (props: any) => {
+  // 弹窗抛出的事件
+  const modalRef = useRef();
+
   const [visible, setVisible] = useState(false);
   const [imgsId, setImgsId] = useState('');
+
+  // 删除/恢复 博文事件
+  const delBowenObj = async (val, id) => {
+    await itinerary._delBowenList({ id, isDelete: !val }).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) =>
+          v
+            .map((item) =>
+              item.id === id ? { ...item, isDelete: !val } : item,
+            )
+            .filter((item) => (+type === 2 ? item.isDelete : !item.isDelete)),
+        );
+        message.success(data.msg);
+        +type === 2
+          ? setTotal((v) => (val ? (v -= 1) : (v += 1)))
+          : setTotal((v) => (val ? (v += 1) : (v -= 1)));
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+  // 彻底删除博文
+  const deleteBowenObj = async (id) => {
+    await itinerary._deleteClassifyDetails({ id }).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) => v.filter((item) => item.id !== id));
+        message.success(data.msg);
+        setTotal((v) => (v -= 1));
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+
   const columns: ColumnsType<DataType> = [
     {
       title: '标题',
@@ -45,9 +91,58 @@ const Classify = () => {
       key: 'title',
       fixed: 'left',
       width: 160,
+      render: (text, record) => (
+        <div className={tableStyle.table_cell}>
+          <Link to={`/itinerary/${record.id}/details`}>{text}</Link>
+        </div>
+      ),
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'timeData',
+      key: 'timeData',
+      width: 190,
       render: (text) => (
         <div className={tableStyle.table_cell}>
-          <Link to="">{text}</Link>
+          {formatDate(text, 'yyyy-MM-dd HH:ss:mm')}
+        </div>
+      ),
+    },
+    {
+      title: '最后修改时间',
+      dataIndex: 'last_edit_time',
+      key: 'last_edit_time',
+      width: 190,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          {formatDate(text, 'yyyy-MM-dd HH:ss:mm')}
+        </div>
+      ),
+    },
+    {
+      title: '状态',
+      dataIndex: 'type',
+      key: 'type',
+      width: 160,
+      render: (text) => (
+        <div className={tableStyle.table_cell}>
+          <SysIcon
+            type={getDictObj('toExamine_type', text)?.icon}
+            style={{ marginRight: '10px' }}
+          />
+          <span
+            className={
+              text === 1
+                ? style.bowenType1
+                : text === 2
+                ? style.bowenType2
+                : text === 3
+                ? style.bowenType3
+                : ''
+            }
+          >
+            {getDictObj('toExamine_type', text)?.value}
+          </span>
         </div>
       ),
     },
@@ -63,17 +158,6 @@ const Classify = () => {
       ),
     },
     {
-      title: '时间',
-      dataIndex: 'timeData',
-      key: 'timeData',
-      width: 160,
-      render: (text) => (
-        <div className={tableStyle.table_cell}>
-          {formatDate(text, 'yyyy-MM-dd')}
-        </div>
-      ),
-    },
-    {
       title: '地点',
       dataIndex: 'place',
       key: 'place',
@@ -85,14 +169,30 @@ const Classify = () => {
       dataIndex: 'weatherId',
       key: 'weatherId',
       width: 100,
-      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+      render: (text) => (
+        <div className={tableStyle.table_cell_flex}>
+          <SysIcon
+            type={getDictObj('weather_list', text)?.icon}
+            className={style.icon}
+          />
+          {getDictObj('weather_list', text)?.value}
+        </div>
+      ),
     },
     {
       title: '心情',
       dataIndex: 'moodId',
       key: 'moodId',
       width: 100,
-      render: (text) => <div className={tableStyle.table_cell}>{text}</div>,
+      render: (text) => (
+        <div className={tableStyle.table_cell_flex}>
+          <SysIcon
+            type={getDictObj('mood_list', text)?.icon}
+            className={style.icon}
+          />
+          {getDictObj('mood_list', text)?.value}
+        </div>
+      ),
     },
 
     {
@@ -129,10 +229,67 @@ const Classify = () => {
       dataIndex: 'operation',
       width: 120,
       fixed: 'right',
-      render: (text) => <div className={tableStyle.table_cell}>操作</div>,
+      render: (text, record) => (
+        <div className={tableStyle.table_cell_flex}>
+          {+type !== 2 && (
+            <Tooltip
+              placement="topRight"
+              arrowPointAtCenter
+              title="修改当前博文审核状态"
+            >
+              <SysIcon
+                type="icon-shenhe"
+                className={style.btn_huifu}
+                onClick={() =>
+                  (modalRef.current as any)?.showModal({
+                    ...record,
+                    type: `${record.type}`,
+                  })
+                }
+              />
+            </Tooltip>
+          )}
+          {+type === 2 ? (
+            <Tooltip
+              placement="topRight"
+              arrowPointAtCenter
+              title="点击后将恢复到列表中"
+            >
+              <UndoOutlined
+                className={style.btn_huifu}
+                onClick={() => delBowenObj(record.isDelete, record.id)}
+              />
+            </Tooltip>
+          ) : (
+            <Popconfirm
+              title="真的要删除吗？删除后将不能在网站查看。"
+              onConfirm={() => delBowenObj(record.isDelete, record.id)}
+              okText="确定"
+              cancelText="取消"
+              placement="topRight"
+              arrowPointAtCenter
+            >
+              <DeleteOutlined className={style.btn_remove} />
+            </Popconfirm>
+          )}
+          {+type === 2 && (
+            <Popconfirm
+              title="将彻底删除该条数据，不可恢复，要继续吗？"
+              onConfirm={() => deleteBowenObj(record.id)}
+              okText="确定"
+              cancelText="取消"
+              placement="topRight"
+              arrowPointAtCenter
+            >
+              <DeleteOutlined className={style.btn_remove} />
+            </Popconfirm>
+          )}
+        </div>
+      ),
     },
   ];
 
+  const { type } = props.location.query as any;
   const [page, setPage] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
   const [total, setTotal] = useState<number>(0);
@@ -155,29 +312,93 @@ const Classify = () => {
     setPageSize(pageSize);
   };
 
+  // 跳转页面事件
+  const changePageType = () => {
+    setPage(1);
+    if (+type === 2) {
+      history.replace('/itinerary/list');
+    } else {
+      history.replace('/itinerary/list?type=2');
+    }
+  };
+
+  // 修改博文审核状态
+  const changeToExamine = async (obj) => {
+    await itinerary._putClassifyToExamine(obj).then(({ data }) => {
+      if (data.code === 200) {
+        setList((v) =>
+          v.map((item) =>
+            item.id === obj.id ? { ...item, type: +obj.type } : item,
+          ),
+        );
+        (modalRef.current as any)?.handleCancel();
+        message.success(data.msg);
+      } else {
+        message.error(data.msg);
+      }
+    });
+  };
+
   useEffect(() => {
     getList();
   }, [page, pageSize]);
 
   return (
-    <Table
-      loading={loading}
-      columns={columns}
-      dataSource={list}
-      rowKey={'id'}
-      scroll={{
-        scrollToFirstRowOnChange: true,
-        x: calcTableScrollWidth(columns),
-        y: `calc(100vh - 220px)`,
-      }}
-      pagination={{
-        current: page,
-        pageSize: pageSize,
-        total: total,
-        showTitle: false,
-      }}
-      onChange={changeTable as any}
-    />
+    <div className={style.classify}>
+      <div className={style.headerBox}>
+        <div className={style.headerBox_left}>
+          <span className={style.page_title}>
+            旅行日记{+type === 2 ? '回收站' : '列表页'}
+          </span>{' '}
+          <span className={style.page_total}>{total}</span>
+        </div>
+        <div className={style.headerBox_right}>
+          <Button
+            type="primary"
+            shape="round"
+            className={style.headerBox_right_btn}
+            href="/itinerary/add-itinerary"
+          >
+            新增旅行日记
+          </Button>
+          <Button
+            type="primary"
+            shape="round"
+            onClick={changePageType}
+            className={
+              +type === 2
+                ? style.headerBox_right_btn
+                : style.headerBox_right_btn1
+            }
+          >
+            {+type === 2 ? '列表页' : '回收站'}
+          </Button>
+        </div>
+      </div>
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={list}
+        rowKey={'id'}
+        scroll={{
+          scrollToFirstRowOnChange: true,
+          x: calcTableScrollWidth(columns),
+          y: `calc(100vh - 220px)`,
+        }}
+        pagination={{
+          current: page,
+          pageSize: pageSize,
+          total: total,
+          showTitle: false,
+        }}
+        onChange={changeTable as any}
+      />
+      <ToExamineModal
+        callback={(ref) => (modalRef.current = ref)}
+        name="type"
+        changeToExamine={changeToExamine}
+      />
+    </div>
   );
 };
 
