f3a7bca9176a9bd83b6c75c9b158b5b2f4208437
王鹏 <75709526+wp0403@users.noreply.github.com>
2022-09-30 16:57:32 +0800

    fix:

--- src/pages/project-library/index.tsx
+++ src/pages/project-library/index.tsx
@@ -4,7 +4,7 @@
  * @Author: WangPeng
  * @Date: 2022-06-08 14:04:40
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-09-08 18:23:51
+ * @LastEditTime: 2022-09-30 16:57:03
  */
 import React, { useEffect } from 'react';
 
@@ -37,51 +37,47 @@ const projectLibrary = () => {
     //   .catch(handleError);
   });
 
-  useEffect(() => {
-    // #获取显示摄像头数据的video
-    const video = document.querySelector('video');
+  // useEffect(() => {
+  //   // #获取显示摄像头数据的video
+  //   const video = document.querySelector('video');
 
-    // #getUserMedia 参数，这里只获取视频
-    const constraints = {
-      audio: false,
-      video: true,
-    };
+  //   // #getUserMedia 参数，这里只获取视频
+  //   const constraints = {
+  //     audio: false,
+  //     video: true,
+  //   };
 
-    // 把摄像头的数据展示在video上
-    function handleSuccess(stream) {
-      window.stream = stream;
-      video.srcObject = stream;
-    }
+  //   // 把摄像头的数据展示在video上
+  //   function handleSuccess(stream) {
+  //     window.stream = stream;
+  //     video.srcObject = stream;
+  //   }
 
-    // #错误处理
-    function handleError(error) {
-      console.log(
-        'navigator.MediaDevices.getUserMedia error: ',
-        error.message,
-        error.name,
-      );
-    }
+  //   // #错误处理
+  //   function handleError(error) {
+  //     console.log(
+  //       'navigator.MediaDevices.getUserMedia error: ',
+  //       error.message,
+  //       error.name,
+  //     );
+  //   }
 
-    // #获取桌面程序数据并回调
-    navigator.mediaDevices
-      .getDisplayMedia(constraints)
-      .then(handleSuccess)
-      .catch(handleError);
+  //   // #获取桌面程序数据并回调
+  //   navigator.mediaDevices
+  //     .getDisplayMedia(constraints)
+  //     .then(handleSuccess)
+  //     .catch(handleError);
 
-    // 设备枚举
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(handleError);
-  });
+  //   // 设备枚举
+  //   navigator.mediaDevices
+  //     .enumerateDevices()
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch(handleError);
+  // });
 
-  return (
-    <div>
-      <video playsInline autoPlay></video>
-    </div>
-  );
+  return <div>{/* <video playsInline autoPlay></video> */}</div>;
 };
 
 export default projectLibrary;
